d23cc035ebd9b86646595b7d938fb30b5652cd1b
Gv-Ramana <gvr907@gmail.com>
2021-09-09 13:39:58 +0530

    Programs got pushed

---
+++ 1-Java/01-java-learn/src/com/java/practice/programs/Main.java
@@ -0,0 +1,21 @@
+package com.java.practice.programs;
+
+public class Main
+{   
+   
+   public static void main(String[] args) {
+		 int arr[]={32,23,121,34,34,23,43,23};
+		 dispay(arr); 
+		 System.out.println(arr);
+		 
+		 
+	}
+	public static int[] dispay(int input[]){
+	    int []arr1=input;
+	    
+	    
+	    return arr1;
+	}
+   
+
+}
---
+++ 1-Java/01-java-learn/src/com/revature/app/accessmodifier/PackageDemo.java
@@ -0,0 +1,10 @@
+package com.revature.app.accessmodifier;
+
+public class PackageDemo {
+
+	public static void main(String[] args) {
+		 
+
+	}
+
+}
